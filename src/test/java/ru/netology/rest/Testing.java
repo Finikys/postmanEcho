f06d668a30af97fd0e4ae7798646e3f39d56275f
Finikys <ForOnceFinikys@gmail.com>
2023-03-02 10:00:33 +0300

    Edit Testing.java

--- src/test/java/ru/netology/rest/Testing.java
+++ src/test/java/ru/netology/rest/Testing.java
@@ -3,6 +3,7 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class Testing {
     @Test
@@ -14,6 +15,7 @@ public class Testing {
                 .post("/post")
                 .then()
                 .statusCode(200)
-        ;
+                .body("data", equalTo("some info"))
+                ;
     }
 }
